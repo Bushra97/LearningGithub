474fb140bffd72939fd814602f50d7edd09c48d4
Bushra97 <32608589+Bushra97@users.noreply.github.com>
2021-12-02 15:10:06 +0530

    Add files via upload

---
+++ DEMO1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_1_Console
+{
+    class DEMO1
+    {
+        public static void Main()
+        {
+            //creating variables
+            string mobile_model;
+            double price;
+            int qty;
+
+            //taking input by default its string
+            Console.WriteLine("Enter mobile model: ");
+            mobile_model = Console.ReadLine();
+            Console.WriteLine("Enter mobile price: ");
+            price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter mobile quantity: ");
+            qty = Convert.ToInt32(Console.ReadLine());
+
+            //printing to the screen
+            Console.WriteLine("The details are {0}, {1}, {2}", mobile_model,price, qty );
+            Console.ReadLine();
+        }
+    }
+}
